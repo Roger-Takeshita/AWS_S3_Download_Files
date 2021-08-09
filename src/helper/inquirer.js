0059c2e80fd1c376c349d2be2a3c25f7bd83d376
Roger Takeshita <roger.takeshita@gmail.com>
2021-08-08 20:15:21 -0400

    feat: Should add inquirer files menu

--- src/helper/inquirer.js
+++ src/helper/inquirer.js
@@ -1,102 +1,47 @@
 const inquirer = require('inquirer');
+const { printError } = require('./print');
 
 inquirer.registerPrompt('checkbox-plus', require('inquirer-checkbox-plus'));
 
-const Phone = [
-    {
-        name: 'XiaoMi',
-        value: 'MI',
-        short: 'MI',
-        disabled: false,
-        type: 'cellphone',
-    },
-    {
-        name: 'Google',
-        value: 'Google',
-        short: 'Google',
-        disabled: true,
-        type: 'cellphone',
-    },
-    {
-        name: 'HUAWEI',
-        value: 'HUAWEI',
-        short: 'HUAWEI',
-        disabled: false,
-        type: 'cellphone',
-    },
-    {
-        name: 'iPhone',
-        value: 'iPhone',
-        short: 'iPhone',
-        disabled: false,
-        type: 'cellphone',
-    },
-];
+const inquirerFiles = async (files) => {
+    const menu = [];
+    const maxFiles = 10;
 
-const iPhone = [
-    { name: 'iPhone 6s', type: 'name', disabled: true },
-    { name: 'iPhone 8 plus', type: 'name', disabled: true },
-    { name: 'iPhone X', type: 'name', disabled: true },
-];
+    for (let i = 0; i < maxFiles; i += 1) {
+        menu.push({
+            name: files[i].filename,
+            value: files[i].url,
+            disabled: false,
+            type: 'url',
+        });
+    }
 
-const XiaoMi = [
-    { name: 'MI note', type: 'name' },
-    { name: 'MI plus', type: 'name' },
-    { name: 'Mi 3', type: 'name' },
-    { name: 'Mi 4', type: 'name' },
-    { name: 'Mi 5', type: 'name' },
-    { name: 'Mi 6', type: 'name' },
-    { name: 'Mi 8', type: 'name' },
-];
-
-const MiNote = [{ name: 'Mi note I', type: 'name' }, { name: 'Mi note II' }, { name: 'Mi note III' }];
-
-const Note = [{ name: 1 }, { name: 2 }];
-
-const MiPlus = [{ name: 'Mi 3 plus' }, { name: 'Mi 4 plus' }, { name: 'Mi 5 plus' }];
-
-inquirer
-    .prompt([
-        {
-            type: 'checkbox-plus',
-            name: 'phone',
-            message: 'select cellphone?',
-            pageSize: 4,
-            highlight: true,
-            default: ['iPhone', 'MI'],
-            footer: 'move up/down to select',
-            header: 'press space to select',
-            searching: 'searching....',
-            noresult: 'nothing..',
-            validate: (answer) => {
-                if (answer.length === 0) {
-                    return 'You must choose at least one phone.';
-                }
-
-                return true;
+    try {
+        const answers = await inquirer.prompt([
+            {
+                type: 'checkbox-plus',
+                name: 'files',
+                message: 'Select your files:',
+                pageSize: 10,
+                highlight: true,
+                default: [menu[0].name],
+                footer: 'move up/down to select',
+                header: 'press space to select',
+                searching: 'searching....',
+                noresult: 'nothing..',
+                validate: (answer) => {
+                    if (answer.length === 0) return 'You must choose at least one file.';
+                    return true;
+                },
+                source: () => Promise.resolve(menu),
             },
-            source: () => Promise.resolve(Phone),
-            subsource: (choice) => {
-                if (choice.name === 'iPhone') {
-                    return Promise.resolve(iPhone);
-                }
-                if (choice.name === 'XiaoMi') {
-                    return Promise.resolve(XiaoMi);
-                }
-                if (choice.name === 'MI note') {
-                    return Promise.resolve(MiNote);
-                }
-                if (choice.name === 'MI plus') {
-                    return Promise.resolve(MiPlus);
-                }
-                if (choice.name === 'Mi note I') {
-                    return Promise.resolve(Note);
-                }
-                return Promise.resolve([]);
-            },
-        },
-    ])
-    .then((answers) => {
-        console.log(answers.phone);
-    })
-    .catch(console.log);
+        ]);
+        return answers.files;
+    } catch (error) {
+        return printError(error);
+    }
+};
+
+module.exports = {
+    inquirerFiles,
+};
--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@
 
 const { getFiles } = require('./aws-s3');
 const { printError } = require('./helper/print');
+const { inquirerFiles } = require('./helper/inquirer');
 
 const getBucketFiles = async () => {
     if (process.argv.length > 2 && process.argv.length <= 3) {
         const files = await getFiles();
+
         files.sort((a, b) => a.filename.localeCompare(b.filename));
-        for (let i = 0; i < 5; i += 1) {
-            console.log(files[i]);
-        }
+        const selectedFiles = await inquirerFiles(files);
+        console.log(selectedFiles);
     } else {
         printError('Please provide an aws profile.');
-        console.log();
     }
 };
 
